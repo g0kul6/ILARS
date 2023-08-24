bcf4983f6652a12d3d7fef084c93ad5ed50fa23c
Gokul <gokul@pop-os.localdomain>
2023-08-24 22:28:46 +0530

    update

--- app.py
+++ app.py
@@ -3,10 +3,22 @@ import gradio as gr
 from app_eval import eval
 
 
-with gr.Blocks() as demo:
+with gr.Blocks(css="footer {visibility: hidden}", title="Surgical-AI") as demo:
+    # Heading and authors
+    gr.Markdown("""
+    <h1 style="text-align: center;">Multimodal Reasoning and Language Prompting for Interactive Learning Assistant for Robotic Surgery. </h1>
+    <h3 style="text-align: center;">Gokul Kannan*, Lalithkumar Seenivasan*, Hongliang Ren** </h3>
+    <h3 style="text-align: center;">labren.org </h3>
+    """)
+    # Video Upload Button
     video_upload = gr.UploadButton(label="Upload the Video", file_types=["video"])
+    # slider
     slider = gr.Slider(maximum=200,interactive=True,steps=1)
+    # image
+    frame_gr = gr.Image(shape=(1280,720))
+    # frames list
     frames = []
+    # function to get individual frames form video
     def get_frame(video):
         frames.clear()
         cap = cv2.VideoCapture(video.name)
@@ -20,26 +32,32 @@ with gr.Blocks() as demo:
             i += 1
         cap.release()
         cv2.destroyAllWindows()
+    # if upload button is triggered 
     video_upload.upload(fn=get_frame, inputs=[video_upload])
+    # function to return slider based indexed frame
     def return_frame(index):
         img = frames[index]
         return img
-    slider.change(return_frame,slider,gr.Image(shape=(1280, 720),type="numpy"))    
+    # if slider is moved
+    slider.change(return_frame,slider,frame_gr)    
+    # question text box
     question = gr.Textbox(label="Question")
-    model_type = gr.CheckboxGroup(["SurgGPT"],label="Model Choice")
-    # model_version = gr.CheckboxGroup(["efvlegpt2Swin","efvlegpt2ViT","efvlegpt2rs18"],label="Model Version")
-    # model_version = "efvlegpt2rs18"
-    # model_type = gr.CheckboxGroup(["LCGN","VisualBERT"],label="Model Choice")
-    # model_version = gr.CheckboxGroup(["vb","vb-rr","lcgn","lcgn-rr"],label="Model Version")
+    # model option checkbox
+    model_type = gr.CheckboxGroup(["Surgical-MmR"],label="Model Choice")
+    # answer textbox
     answer = gr.Textbox(label="Answer")
+    # LLM description textbox
     description = gr.Textbox(label="Description")
+    # Predict Button
     predict = gr.Button(value="Predict")
+    # predict ans using VQA model
     def predict_ans(index,question,model_choice):
         img = frames[index]
         ans,description = eval(model_ver="efvlegpt2rs18",img=img,question=question)
-        # ans = "hi"
         return ans,description
+    # predict button is triggered
     predict.click(fn=predict_ans,inputs=[slider,question,model_type],outputs=[answer,description])
     
+# launch the gradio app
 if __name__ == "__main__":
-    demo.launch()
+    demo.launch(share=True)
